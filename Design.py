191e927e0f74c486a1c8c3217ee1dfb673d5a97d
Zachary A Carmichael <ZacAC2024@outlook.com>
2024-12-13 15:46:41 -0700

    Added Credits, apple 2 classes now switch to apple 1

--- Design.py
+++ Design.py
@@ -1,20 +1,25 @@
 import tkinter as tk
+# import customtkinter as ctk
 from tkinter import scrolledtext, messagebox, ttk
 from bs4 import BeautifulSoup
 
+
+
 # To install the necessary libraries:
+# Download Python 3.11 or higher on the Microsoft Store
 # open command prompt
 # run 'pip install beautifulsoup4'
 
 
 '''
     TODO:
-    - configure Git identity - https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
+    ✔ configure Git identity - https://git-scm.com/book/en/v2/Getting-Started-First-Time-Git-Setup
     - add NMSU-A Theme
-    - add Title: NMSU-A: DesignPlus Parser
+    ✔ add Title: NMSU-A: DesignPlus Parser
     - add a credits to Zachary A. Carmichael - Administrative Assistant
     - change from tk to CTk
-    - make automatically change the HTML Class to apple variation_1
+    ✔ make automatically change the HTML Class to apple variation_1
+    - Package it for distribution
 
 '''
 
@@ -25,7 +30,7 @@ from bs4 import BeautifulSoup
 
 #______Code_Formatting______________________
 # intializes variables to hold the needed input code
-appleClass = 'class="kl_apple variation_1 kl_wrapper"'
+appleClass = "kl_apple variation_1 kl_wrapper"
 progressBar = '<p class="kl_module_progress_bar" style="display: none; color: #000000; background-color: #00b9f2;">Basic Progress Bar (built in browser, hidden in app)</p>\n'
 crimsonBar = 'border-top-width: 3px; border-top-color: #882345;'
 
@@ -40,6 +45,9 @@ def modifyCode(htmlCode):
 
         for h3 in soup.find_all('h3'):
             h3['style'] = crimsonBar        # uses 'style' because there is special functionality in BeautifulSoup that lets you edit HTML code
+
+        for class_ in soup.find_all(class_="kl_apple variation_2 kl_wrapper"):
+            class_['class'] = appleClass
         
         return str(soup)
     except Exception as e:
@@ -60,8 +68,8 @@ def formatCode():
 # Creates the main window
 windowA = tk.Tk()
 windowA.iconbitmap('Icon.ico')
-windowA.title("DesignPlus Formatting")
-windowA.geometry("800x600")
+windowA.title("NMSU-A: DesignPlus Parser")
+windowA.geometry("800x620")
 
 # Create a frame for styling the main content
 mainFrame = ttk.Frame(windowA, padding="10")
@@ -89,6 +97,11 @@ outputHeader.pack()
 # makes a scrollable textbox for the formatted code
 outputCode = scrolledtext.ScrolledText(mainFrame, wrap=tk.WORD, width=100, height=15)
 outputCode.pack()
+
+# Credits Label
+italicFont = ("Arial", 8, "italic")
+creditLable = tk.Label(mainFrame, text="Created by Zachary A. Carmichael - ZacAC2024@outlook.com", font=italicFont, fg="gray")
+creditLable.pack(side="bottom", anchor="se", pady=10)
 
 
 windowA.mainloop()  # Causes the window to stay open
